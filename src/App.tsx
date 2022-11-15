4316cb1115dff9e07f84d129a34b5eda0e3673e5
EricaReis <ericareis67@outlook.com>
2022-11-15 08:50:39 -0300

    Add radix slot

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ export function App() {
   return (
     <div className="w-screen h-screen bg-zinc-900 flex items-center justify-center gap-4">
       <Button>Create account</Button>
-      <Button>Create account</Button>
+      <Button asChild>
+        <a href="#">Link</a>
+      </Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 import React, { ButtonHTMLAttributes } from "react";
+import { Slot } from "@radix-ui/react-slot";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+}
 
 export function Button(props: ButtonProps) {
+  const Component = props.asChild ? Slot : "button";
+
   return (
-    <button
+    <Component
       className="px-4 py-2 rounded bg-violet-400 text-white font-bold"
       {...props}
     />
